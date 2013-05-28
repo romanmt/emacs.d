9eb4a482be02fba349036d90d7ebd3e2ae6ce9b2
Matt Roman <romanmt@gmail.com>
2013-05-27 22:19:59 -0400

    Fixed key bindings

--- core/key-bindings.el
+++ core/key-bindings.el
@@ -2,6 +2,6 @@
 (global-set-key (kbd "C-' C-f") 'find-file)
 (global-set-key (kbd "C-' C-g") 'magit-status)
 (global-set-key (kbd "C-' C-a") 'ack-and-a-half)
-(global-set-key (kbd "C-' C-t") (kbd "M-<"))
-(global-set-key (kbd "C-' C-b") (kbd "M->"))
+(global-set-key (kbd "C-' C-s") (kbd "C-c s"))
+
 (provide 'key-bindings)
