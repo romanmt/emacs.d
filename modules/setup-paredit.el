cc1a67346b70149cc3afdd11a26f7167ca0b3088
Matt Roman <romanmt@gmail.com>
2013-07-29 15:27:01 -0400

    load smartparens on js2-mode

---
+++ modules/setup-paredit.el
@@ -0,0 +1,10 @@
+(require 'paredit)
+
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'scheme-mode-hook           (lambda () (paredit-mode +1)))
+(add-hook 'geiser-repl-mode-hook      (lambda () (paredit-mode +1)))
+(add-hook 'ielm-mode-hook             (lambda () (paredit-mode +1)))
+
+(provide 'setup-paredit)
--- modules/setup-smartparens.el
+++ modules/setup-smartparens.el
@@ -1,8 +1,11 @@
 (show-smartparens-global-mode t)
 
+
 ;(setq sp-highlight-pair-overlay nil)
 ;(setq sp-highlight-wrap-overlay nil)
 ;(setq sp-highlight-wrap-tag-overlay nil)
 ;(setq sp-navigate-consider-symbols t)
+
+(add-hook 'js2-mode-hook (lambda () (smartparens-mode 1)))
 
 (provide 'setup-smartparens)
