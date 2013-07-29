38d82def79e78fa31466c4ec85d991ee2c2db673
Matt Roman <romanmt@gmail.com>
2013-07-29 10:17:11 -0400

    fixes up js2 mode, adds snippets and some other nice stuff

---
+++ modules/setup-smartparens.el
@@ -0,0 +1,8 @@
+(show-smartparens-global-mode t)
+
+;(setq sp-highlight-pair-overlay nil)
+;(setq sp-highlight-wrap-overlay nil)
+;(setq sp-highlight-wrap-tag-overlay nil)
+;(setq sp-navigate-consider-symbols t)
+
+(provide 'setup-smartparens)
