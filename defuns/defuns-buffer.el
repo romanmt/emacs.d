111a0eeefcc89d5d873e910a1ae483f60402e015
Matt Roman <romanmt@gmail.com>
2013-10-10 08:35:47 -0400

    Adds functions for cleaning the current buffer

---
+++ defuns/defuns-buffer.el
@@ -0,0 +1,24 @@
+(defun clean-and-save-buffer ()
+  "Does some basic cleanup on the buffer before saving."
+  (cleanup-buffer)
+  (save-buffer))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (untabify-buffer)
+  (whitespace-cleanup)
+  (indent-buffer))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+
+
+
